eceb7878b359928497be9102c41a7b8732efc1ee
Mikael Souza Da França Falcão <51915337801@info.local>
2023-05-03 16:13:55 -0300

    Boleto

---
+++ Boleto.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace passed_rod_project
+{
+    public class Boleto
+    {
+        public float DescontoBoleto(float valor)
+        {
+            return (valor / 0.88f);
+        }
+    }
+}
