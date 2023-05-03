4e6cb41d032e628c153364421cc66c67623cce3a
Kauã Rodrigues <kaua.rodrigues2620@gmail.com>
2023-05-03 16:52:43 -0300

    resolve conflito

--- Program.cs
+++ Program.cs
@@ -5,8 +5,11 @@ Credito c = new Credito();
 Debito d = new Debito();
 Boleto b = new Boleto();
 
-Console.ForegroundColor = ConsoleColor.Green;
-Console.WriteLine($@"
+string opcao;
+do
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($@"
 ============================================
 Escolha a sua forma de pagamento:
 [1] - Cartão de crédito.
@@ -15,33 +18,34 @@ Escolha a sua forma de pagamento:
 [0] - Cancelar operação.
 =============================================
 ");
-Console.ResetColor();
-string opcao = Console.ReadLine()!;
+    Console.ResetColor();
+    opcao = Console.ReadLine()!;
 
-switch (opcao)
-{
-    case "0":
-        p.Cancelar();
-        break;
-    case "1":
-        Console.WriteLine($"Informe o valor da compra: ");
-        c.valor = int.Parse(Console.ReadLine()!);
-        c.SalvarCartao();
-        c.Pagar();
-        break;
-    case "2":
-        Console.WriteLine($"Informe o valor da compra: ");
-        d.valor = int.Parse(Console.ReadLine()!);
-        d.SalvarCartao();
-        d.Pagar();
-        break;
-    case "3":
-        Console.WriteLine($"Informe o valor da compra: ");
-        b.valor = int.Parse(Console.ReadLine()!);
-        b.Registrar();
-        b.Cancelar();
-        break;
-    default:
-        Console.WriteLine($"Opção inválida.");
-        break;
-}
+    switch (opcao)
+    {
+        case "0":
+            p.Cancelar();
+            break;
+        case "1":
+            Console.WriteLine($"Informe o valor da compra: ");
+            c.valor = int.Parse(Console.ReadLine()!);
+            c.SalvarCartao();
+            c.Pagar();
+            break;
+        case "2":
+            Console.WriteLine($"Informe o valor da compra: ");
+            d.valor = int.Parse(Console.ReadLine()!);
+            d.SalvarCartao();
+            d.Pagar();
+            break;
+        case "3":
+            Console.WriteLine($"Informe o valor da compra: ");
+            b.valor = int.Parse(Console.ReadLine()!);
+            b.Registrar();
+            b.Cancelar();
+            break;
+        default:
+            Console.WriteLine($"Opção inválida.");
+            break;
+    }
+} while (opcao != "0");
